f6cf052812adac025b887795be8163ec7fe3981f
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2022-03-26 14:12:32 +0500

    ALL PROBLEM SOLVED EVEN IMAGES ARE SERVED

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(expressEjsLayouts)
 app.set('layout','layouts/layout');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.set('./', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 RoutingRegisteration(app);
 
